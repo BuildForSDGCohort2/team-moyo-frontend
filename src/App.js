e86be2efca4707003bfb977ba880dd15d58b71d3
Nyamador <dselasi4u@gmail.com>
2020-08-24 21:18:04 +0000

    feat: added initial datamodel for firebase as dataModels.json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Moyo</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Moyo</h1>
+		</div>
+	);
 }
 
 export default App;
