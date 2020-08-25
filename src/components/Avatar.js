b71ba54a38a1e180503e27321448c664d5eee354
Nyamador <dselasi4u@gmail.com>
2020-08-25 22:35:18 +0000

    Added Avatar Component

---
+++ src/components/Avatar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Avatar.css';
+
+const Avatar = ({ url, alt, ...props }) => {
+	return (
+		<div>
+			<img src={url} alt={alt} {...props} className="moyo-avatar" />
+		</div>
+	);
+};
+
+Avatar.propTypes = {
+	/**
+	 * The URL to the image
+	 */
+	url: PropTypes.string.isRequired,
+	/**
+	 * The alt attribute of the Avatar
+	 */
+	title: PropTypes.string,
+};
+
+Avatar.defaultProps = {
+	alt: 'Image may contain - Moyo User',
+};
+export default Avatar;
