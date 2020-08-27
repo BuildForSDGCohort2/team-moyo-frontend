de63bbba4b0d32511dbb7c324723641be521ff66
momotsow <kgomotsomkansi@gmail.com>
2020-08-28 01:28:22 +0200

    added a banner logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import  LandingPage from "./components/Landing-page/index";
+
 function App() {
   return (
     <div className="App">
-      <h1>Moyo</h1>
+      <div>
+      {/* <h1>Moyo</h1> */}
+      <LandingPage />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Landing-page/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import logo from '../../assets/img/logo.png'; 
+import '../../App.css';
+export default class LandingPage extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <div className="banner">
+        <img className="" src={logo} alt="Logo" />
+        </div>
+
+        
+      </div>
+    )
+  }
+}
